0e20bd2a8614ef160add2ad0fa21c8ec55dd4241
Jack <jack.naisbett@gmail.com>
2020-01-05 16:46:06 +0000

    Always check for new ASP page

--- uiScribe.sh
+++ uiScribe.sh
@@ -104,8 +104,9 @@ Update_Version(){
 			Print_Output "true" "MD5 hash of $SCRIPT_NAME does not match - downloading updated $serverver" "$PASS"
 		fi
 		
+		Update_File "Main_LogStatus_Content.asp"
+		
 		if [ "$doupdate" != "false" ]; then
-			Update_File "Main_LogStatus_Content.asp"
 			/usr/sbin/curl -fsL --retry 3 "$SCRIPT_REPO/$SCRIPT_NAME.sh" -o "/jffs/scripts/$SCRIPT_NAME" && Print_Output "true" "$SCRIPT_NAME successfully updated"
 			chmod 0755 /jffs/scripts/"$SCRIPT_NAME"
 			Clear_Lock
