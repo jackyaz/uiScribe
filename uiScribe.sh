8fc5b7d1a53c813458c71b59cda021a5bb6bf1e6
Jack <jack.naisbett@gmail.com>
2020-04-25 11:14:57 +0100

    Version bump, remove old variables

--- uiScribe.sh
+++ uiScribe.sh
@@ -15,16 +15,13 @@
 
 ### Start of script variables ###
 readonly SCRIPT_NAME="uiScribe"
-readonly SCRIPT_VERSION="v1.2.1"
-readonly SCRIPT_BRANCH="master"
+readonly SCRIPT_VERSION="v1.3.0"
+readonly SCRIPT_BRANCH="develop"
 readonly SCRIPT_REPO="https://raw.githubusercontent.com/jackyaz/""$SCRIPT_NAME""/""$SCRIPT_BRANCH"
-readonly OLD_SCRIPT_DIR="/jffs/scripts/$SCRIPT_NAME.d"
 readonly SCRIPT_DIR="/jffs/addons/$SCRIPT_NAME.d"
-readonly OLD_SCRIPT_CONF="/jffs/configs/$SCRIPT_NAME.config"
 readonly SCRIPT_CONF="$SCRIPT_DIR/config"
 readonly SCRIPT_PAGE_DIR="$(readlink /www/user)"
 readonly SCRIPT_WEB_DIR="$SCRIPT_PAGE_DIR/$SCRIPT_NAME"
-readonly OLD_SHARED_DIR="/jffs/scripts/shared-jy"
 readonly SHARED_DIR="/jffs/addons/shared-jy"
 readonly SHARED_REPO="https://raw.githubusercontent.com/jackyaz/shared-jy/master"
 [ -z "$(nvram get odmpid)" ] && ROUTER_MODEL=$(nvram get productid) || ROUTER_MODEL=$(nvram get odmpid)
@@ -157,22 +154,8 @@ Create_Dirs(){
 		mkdir -p "$SCRIPT_DIR"
 	fi
 	
-	if [ -d "$OLD_SCRIPT_DIR" ]; then
-		mv "$OLD_SCRIPT_DIR" "$(dirname "$SCRIPT_DIR")"
-		rm -rf "$OLD_SCRIPT_DIR"
-	fi
-	
-	if [ -f "$OLD_SCRIPT_CONF" ]; then
-		mv "$OLD_SCRIPT_CONF" "$SCRIPT_CONF"
-	fi
-	
 	if [ ! -d "$SHARED_DIR" ]; then
 		mkdir -p "$SHARED_DIR"
-	fi
-	
-	if [ -d "$OLD_SHARED_DIR" ]; then
-		mv "$OLD_SHARED_DIR" "$(dirname "$SHARED_DIR")"
-		rm -rf "$OLD_SHARED_DIR"
 	fi
 	
 	if [ ! -d "$SCRIPT_WEB_DIR" ]; then
